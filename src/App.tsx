9031ad4280e46422eff43233021f5e36f2fcd0e8
RafaelMatos <rafael-matos@outlook.com>
2023-09-17 18:46:07 -0300

    ai gerando resposta pra video carregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 import { Button } from "./components/ui/button";
-import {  Github, Wand2 } from "lucide-react";
+import { Github, Wand2 } from "lucide-react";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
+import { useCompletion } from "ai/react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  function handlePromptSelected(template: string) {
+    console.log(template);
+  }
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -30,11 +63,14 @@ export function App() {
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
               readOnly
+              value={completion}
             />
           </div>
           <p className="text-sm text-muted-foreground">
@@ -45,42 +81,48 @@ export function App() {
           </p>
         </div>
         <aside className="w-80 space-y-6">
-          <VideoInputForm/>
+          <VideoInputForm onVideoUploaded={setVideoId} />
           <Separator />
-          <form className="space-y-6">
-          <div className="space-y-2">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="space-y-2">
               <Label>Prompt</Label>
-              <PromptSelect />
+              <PromptSelect onPromptSelected={setInput} />
             </div>
             <div className="space-y-2">
               <Label>Modelo</Label>
               <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
-                  <SelectValue/>
+                  <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="text-muted-foreground text-xs italic block">Você poderá customizar essa opção em breve</span>
+              <span className="text-muted-foreground text-xs italic block">
+                Você poderá customizar essa opção em breve
+              </span>
             </div>
-            <Separator/>
+            <Separator />
             <div className="space-y-4">
               <Label>Temperatura</Label>
               <Slider
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
               />
-              <span className="text-muted-foreground text-xs italic block">Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.</span>
+              <span className="text-muted-foreground text-xs italic block">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
+              </span>
             </div>
-
-          </form>
-          <Separator/>
-          <Button className="w-full" type="submit">
+          <Separator />
+          <Button disabled={isLoading} type="submit" className="w-full">
             Executar
-            <Wand2 className="w-4 h-4 ml-2"/>
+            <Wand2 className="w-4 h-4 ml-2" />
           </Button>
+          </form>
         </aside>
       </main>
     </div>
